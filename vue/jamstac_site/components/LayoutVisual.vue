9b6f1b84bb185f8beda22c135ebeec9d9b785b1f
ryoiwaki <81896233+ryoiwaki@users.noreply.github.com>
2023-03-29 18:46:23 +0900

    Create Jamstack site

--- vue/jamstac_site/components/LayoutVisual.vue
+++ vue/jamstac_site/components/LayoutVisual.vue
@@ -20,25 +20,6 @@
         </div>
       </div>
     </div>
-    <div
-      class="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden"
-      style="height: 70px; transform: translateZ(0px)"
-    >
-      <svg
-        class="absolute bottom-0 overflow-hidden"
-        xmlns="http://www.w3.org/2000/svg"
-        preserveAspectRatio="none"
-        version="1.1"
-        viewBox="0 0 2560 100"
-        x="0"
-        y="0"
-      >
-        <polygon
-          class="text-white fill-current"
-          points="2560 0 2560 100 0 100"
-        ></polygon>
-      </svg>
-    </div>
   </div>
 </template>
 
